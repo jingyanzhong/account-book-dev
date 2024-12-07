07183dc56a83d189d7e6343b9d04502ac8bb0523
TiramiAsu <tiramisu0116@gmail.com>
2024-12-07 12:53:59 +0800

    [ DB ] 調整 schema 與新增初始資料及測試資料

--- SQL/1_tables.sql
+++ SQL/1_tables.sql
@@ -1,6 +1,5 @@
 --會計科目檔
-CREATE TABLE public.SUBJECT
-(
+CREATE TABLE public.SUBJECT (
     CODE character varying(4) NOT NULL,
     NAME character varying(20) NOT NULL,
     CREATE_TIME timestamp without time zone NOT NULL DEFAULT now(),
@@ -24,14 +23,15 @@ COMMENT ON COLUMN public.SUBJECT.CREATE_TIME
 
 --日記帳檔
 CREATE TABLE public.JOURNAL_RECORD (
-	JOURNAL_RECORD_KEY bigserial NOT NULL,
+    JOURNAL_RECORD_KEY bigserial NOT NULL,
     PRIMARY KEY (JOURNAL_RECORD_KEY),
-	TX_TIME timestamp without time zone NOT NULL,
-	DEBIT character varying(4) NOT NULL,
-	CREDIT character varying(4) NOT NULL,
-	AMOUNT bigint NOT NULL,
-	MEMO character varying(10) NOT NULL,
-	CREATE_TIME timestamp without time zone NULL DEFAULT now()
+    TX_TIME timestamp without time zone NOT NULL,
+    DEBIT character varying(4) NOT NULL,
+    CREDIT character varying(4) NOT NULL,
+    AMOUNT bigint NOT NULL,
+    MEMO character varying(10) NOT NULL,
+    REMARK character varying(50) NULL,
+    CREATE_TIME timestamp without time zone NULL DEFAULT now()
 );
 
 ALTER TABLE IF EXISTS public.JOURNAL_RECORD
@@ -60,3 +60,28 @@ COMMENT ON COLUMN public.JOURNAL_RECORD.MEMO
 
 COMMENT ON COLUMN public.JOURNAL_RECORD.CREATE_TIME
     IS '建立時間';
+
+
+--日記帳摘要檔
+CREATE TABLE public.JOURNAL_MEMO (
+    CODE character varying(4) NOT NULL,
+    NAME character varying(20) NOT NULL,
+    CREATE_TIME timestamp without time zone NOT NULL DEFAULT now(),
+    PRIMARY KEY (CODE)
+);
+
+ALTER TABLE IF EXISTS public.JOURNAL_MEMO
+    OWNER to postgres;
+
+COMMENT ON TABLE public.JOURNAL_MEMO
+    IS '日記帳摘要檔';
+
+COMMENT ON COLUMN public.JOURNAL_MEMO.CODE
+    IS '日記帳摘要代號';
+
+COMMENT ON COLUMN public.JOURNAL_MEMO.NAME
+    IS '日記帳摘要名稱';
+
+COMMENT ON COLUMN public.JOURNAL_MEMO.CREATE_TIME
+    IS '建立時間';
+
---
+++ SQL/3_procedure.sql
@@ -0,0 +1,59 @@
+
+
+-- 取得每月貸項加總
+CREATE OR REPLACE FUNCTION get_monthly_summary_debit(p_year INT, p_month INT)
+RETURNS TABLE (
+    year INT,
+    month INT,
+    code character varying(4),
+    sum_amount NUMERIC
+) as $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        to_char(j.tx_time, 'YYYY')::INT as year,
+        to_char(j.tx_time, 'MM')::INT as month, 
+        s.code as code,
+        SUM(amount) as sum_amount
+    from
+        subject s, journal_record j
+    WHERE
+        j.debit = s.code AND
+        to_char(j.tx_time, 'YYYY')::INT = p_year AND
+        to_char(j.tx_time, 'MM')::INT = p_month
+    GROUP BY
+        to_char(j.tx_time, 'YYYY'), to_char(j.tx_time, 'MM'), s.code
+    ORDER BY
+        to_char(j.tx_time, 'YYYY'), to_char(j.tx_time, 'MM'), s.code;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- 取得每月借項加總
+CREATE OR REPLACE FUNCTION get_monthly_summary_credit(p_year INT, p_month INT)
+RETURNS TABLE (
+    year INT,
+    month INT,
+    code character varying(4),
+    sum_amount NUMERIC
+) as $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        to_char(j.tx_time, 'YYYY')::INT as year,
+        to_char(j.tx_time, 'MM')::INT as month, 
+        s.code as code,
+        SUM(amount) as sum_amount
+    from
+        subject s, journal_record j
+    WHERE
+        j.credit = s.code AND
+        to_char(j.tx_time, 'YYYY')::INT = p_year AND
+        to_char(j.tx_time, 'MM')::INT = p_month
+    GROUP BY
+        to_char(j.tx_time, 'YYYY'), to_char(j.tx_time, 'MM'), s.code
+    ORDER BY
+        to_char(j.tx_time, 'YYYY'), to_char(j.tx_time, 'MM'), s.code;
+END;
+$$ LANGUAGE plpgsql;
+
